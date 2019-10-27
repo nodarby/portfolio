7cc483abfb40142b429ebea0e5bc4158ae78cab0
nodarby <ndtmhr@outlook.jp>
2019-10-27 21:45:50 +0900

    Profile追記

--- src/Contact.js
+++ src/Contact.js
@@ -9,7 +9,12 @@ function Contact() {
     return (
         <div className="App">
             <h2 className="main">Contact</h2>
-            <div className="main"><div className="jap"> メールアドレス</div>：<div className="eng_line"> noda.tomohiro@h.mbox.nagoya-u.ac.jp</div></div>
+            <div className="main">
+                <div className="jap-in"> メールアドレス</div>：<div className="eng"> noda.tomohiro@h.mbox.nagoya-u.ac.jp</div>
+            </div>
+            <div className="main">
+                <div className="jap-in"> github</div>　　　　：<a className="eng_line" href={"https://github.com/nodarby"}>https://github.com/nodarby</a>
+            </div>
         </div>
     );
 }
